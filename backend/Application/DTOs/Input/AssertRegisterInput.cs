afb418f20f9b5234e79f2531bf2feb8d9e1d75cc
Fernando iMac <fersants.nando.ferreira@gmail.com>
2024-01-16 17:54:27 -0300

    update

--- backend/Application/DTOs/Input/AssertRegisterInput.cs
+++ backend/Application/DTOs/Input/AssertRegisterInput.cs
@@ -7,12 +7,9 @@ namespace Application.DTOs.Input
     public class AssertRegisterOptions
     {
         [Required]
-        [EmailAddress]
-        [Display(Name = "Email")]
         public string Username { get; set; }
 
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 1)]
-        [Display(Name = "Name")]
         public string DisplayName { get; set; }
 
         [Required]
--- backend/Application/DTOs/Input/CreateRegisterOptionsInput.cs
+++ backend/Application/DTOs/Input/CreateRegisterOptionsInput.cs
@@ -10,13 +10,9 @@ namespace Application.DTOs.Input
         ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
         ///     directly from your code. This API may change or be removed in future releases.
         /// </summary>
-        [Required]
-        [EmailAddress]
-        [Display(Name = "Email")]
         public string Username { get; set; }
 
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 1)]
-        [Display(Name = "Name")]
         public string DisplayName { get; set; }
 
         public string? AttestationResponse { get; set; }
